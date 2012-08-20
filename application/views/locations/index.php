36a079cc50dba5a9f398f2e980a05fd8cc7ab995
Matthew Purdon <matthew@codenaked.org>
2012-08-20 16:13:53 -0400

    bc expects spaces to be %20 rather than +

--- application/views/locations/index.php
+++ application/views/locations/index.php
@@ -15,7 +15,7 @@
 	<tbody>
 	<?php foreach ($locations as $location):?>
 		<tr>
-			<td><?php echo $location->pilot; ?><span class="bc_link"><a href="http://eve.battleclinic.com/killboard/combat_record.php?type=player&name=<?php echo urlencode($location->pilot); ?>#losses" target="_new">[bc]</a></span></span><span class="search_link"><?php echo anchor(current_url() . '?pilot=' . $location->pilot, '[?]'); ?></span></td>
+			<td><?php echo $location->pilot; ?><span class="bc_link"><a href="http://eve.battleclinic.com/killboard/combat_record.php?type=player&name=<?php echo rawurlencode($location->pilot); ?>#losses" target="_new">[bc]</a></span></span><span class="search_link"><?php echo anchor(current_url() . '?pilot=' . $location->pilot, '[?]'); ?></span></td>
 			<td><?php echo timespan(strtotime($location->date)),' ago'; ?></td>
 			<td><?php echo $location->station; ?></td>
 			<td><span class="set_system"><a href="#" id="<?php echo $location->system_id; ?>"><?php echo $location->system; ?></a></span><span class="search_link"><?php echo anchor(current_url() . '?system=' . $location->system, '[?]'); ?></span></td>

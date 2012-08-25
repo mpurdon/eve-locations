ba65fa750aa60f00c58dbbc1c702ae18292650be
Matthew Purdon <matthew@codenaked.org>
2012-08-25 19:29:21 -0400

    Removed the pilot name validation rules because I am too lazy to write a callback.

--- application/controllers/locations.php
+++ application/controllers/locations.php
@@ -83,7 +83,6 @@ class Locations extends MY_Controller {
 		$this->load->helper('form');
 		$this->load->library('form_validation');
 
-		$this->form_validation->set_rules('pilot', 'Pilot', 'alpha_dash');
 		$this->form_validation->set_rules('system', 'System', 'alpha_dash');
 		$this->form_validation->set_rules('constellation', 'Constellation', 'alpha_dash');
 		$this->form_validation->set_rules('region', 'Region', 'alpha_dash');

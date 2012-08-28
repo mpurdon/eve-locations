bfcab76de859b5a4874f4ad13552f1e503c7a90a
Matthew Purdon <matthew@codenaked.org>
2012-08-28 18:56:18 -0400

    Added terms of use and my emailz0r

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,2 +1,5 @@
+	<div id="footer">
+		<p>&copy; 2012 <a href="mailto:chuck@chuckstalker.com">Chuck Stalker</a>. This site is afraid of being sued so we have <a href="#" id="show-terms">these terms</a> for you to read.</p>
+	</div>
 </body>
 </html>
--- static/js/eve-locations.js
+++ static/js/eve-locations.js
@@ -91,6 +91,10 @@ function notifyUpdates() {
 	}
 }
 
+function displayTerms() {
+	apprise('EVE Online and the EVE logo are the registered trademarks of CCP hf. All rights are reserved worldwide. All other trademarks are the property of their respective owners. EVE Online, the EVE logo, EVE and all associated logos and designs are the intellectual property of CCP hf. All artwork, screenshots, characters, vehicles, storylines, world facts or other recognizable features of the intellectual property relating to these trademarks are likewise the intellectual property of CCP hf. CCP hf. has granted permission to evewho.com to use EVE Online and all associated logos and designs for promotional and information purposes on its website but does not endorse, and is not in any way affiliated with, evewho.com. CCP is in no way responsible for the content on or functioning of this website, nor can it be liable for any damage arising from the use of this website.');
+}
+
 $(document).ready(function() {
 	
 	// Handle browser trust
@@ -171,5 +175,9 @@ $(document).ready(function() {
 		$('#system').val('wormhole');
 		$('#constellation').val('');
 		$('#region').val('');
+	});
+	
+	$('#show-terms').click(function(event) {
+		displayTerms();
 	});
 });
